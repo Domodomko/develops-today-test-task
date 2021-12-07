996846a1684e8d86953484a5d303432e8b8363a0
Sergei Shinn <55379976+Domodomko@users.noreply.github.com>
2021-12-07 23:53:25 +0600

    Update settings.py

--- develops_today/settings.py
+++ develops_today/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 from decouple import config
 from psycopg2.extensions import ISOLATION_LEVEL_READ_COMMITTED
@@ -129,8 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
